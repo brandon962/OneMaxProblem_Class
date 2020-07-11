75e5f7dd9a54f2886af4a2f02c5f00261166b814
brandon962 <40123051+brandon962@users.noreply.github.com>
2020-07-11 13:22:51 +0800

    QQ

--- OneMaxProblem_Class/OneMaxProblem_Class.cpp
+++ OneMaxProblem_Class/OneMaxProblem_Class.cpp
@@ -1,28 +1,83 @@
 ﻿#define _CRT_SECURE_NO_DEPRECATE
 #define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
 #include <iostream>
+#include <cstring>
 #include "mybitset.h"
 #include "es.h"
 #include "hclr.h"
 #include "hc.h"
 #include "sa.h"
+#include "ts.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    
-<<<<<<< Updated upstream
-    HcLr test = HcLr(30, 3000, 100);
-    test.run();
-=======
-    cout << endl;
-    //cout << "so how about this" << endl;
-    return 0;
->>>>>>> Stashed changes
+    int runs = 30;
+    int bits = 100;
+    int iterations = 1000;
+    string algo;
+    double temperature = 1.2;
+    double alpha = 0.97;
+    int list_long = 10;
 
-    Sa test1 = Sa(30, 3000, 100);
-    test1.run();
-    system("PAUSE");
+    if (argc > 1)
+        algo = argv[1];
+
+    if (algo == "hc") {
+        if(argc > 4) {
+            runs = atoi(argv[2]);
+            iterations = atoi(argv[3]);
+            bits = atoi(argv[4]);
+        }
+        Hc hc = Hc(runs, iterations, bits);
+        hc.run();
+    }
+    else if (algo == "hclr") {
+        if (argc > 4) {
+            runs = atoi(argv[2]);
+            iterations = atoi(argv[3]);
+            bits = atoi(argv[4]);
+        }
+        HcLr hclr = HcLr(runs, iterations, bits);
+        hclr.run();
+    }
+    else if (algo == "sa") {
+        if (argc > 6) {
+            runs = atoi(argv[2]);
+            iterations = atoi(argv[3]);
+            bits = atoi(argv[4]);
+            temperature = atoi(argv[5]);
+            alpha = atoi(argv[6]);
+        }
+        Sa sa = Sa(runs, iterations, bits, temperature, alpha);
+        sa.run();
+    }
+    else if (algo == "ts") {
+        if (argc > 5) {
+            runs = atoi(argv[2]);
+            iterations = atoi(argv[3]);
+            bits = atoi(argv[4]);
+            list_long = atoi(argv[5]);
+        }
+        Ts ts = Ts(runs, iterations, bits, list_long);
+        ts.run();
+    }
+    else
+    {
+        /*Hc hc = Hc(runs, iterations, bits);
+        hc.run();
+        HcLr hclr = HcLr(runs, iterations, bits);
+        hclr.run();*/
+        Sa sa = Sa(runs, iterations, bits, temperature, alpha);
+        sa.run();
+        Ts ts = Ts(runs, iterations, bits, list_long);
+        ts.run();
+    }
+    
+    cout << endl;
+    cout << "so how about this" << endl;
+    return 0;
+
 }
 
